f2c01d51530c397a45f1244b0e76240b50f30065
JIgorrr <57548311+JIgorrr@users.noreply.github.com>
2021-10-04 21:23:41 -0300

    Update and rename conversos de temperatura to conversosdetemperatura.py

--- conversosdetemperatura.py
+++ conversosdetemperatura.py
@@ -5,7 +5,7 @@ c = 32
 def converter():
     d = (f - c) / 1.8
 
-    print("Sua temperatura em Graus Celsius é: {:.2F}°C".format(d))
+    print("Sua temperatura em Graus Celsius é: {:.2F}°C".format(ds
 
 
 converter()
